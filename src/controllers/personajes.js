aa2b9219fddfd4ded0d05db0e42630dc85ddd4e8
Agustin Gonzalez <agustin-ag@live.com>
2022-06-21 20:27:39 +0300

    Added endpoint PUT /characters

--- src/controllers/personajes.js
+++ src/controllers/personajes.js
@@ -49,4 +49,29 @@ personajesController.delete('/:id', async (req, res, next) => {
   }
 })
 
+personajesController.put('/:id', async (req, res, next) => {
+  const id = req.params.id
+  console.log(id)
+  const personajeData = {
+    imagen: req.body.imagen,
+    nombre: req.body.nombre,
+    edad: req.body.edad,
+    peso: req.body.peso,
+    historia: req.body.historia,
+    peliculasOSeries: req.body.peliculasOSeries
+  }
+  try {
+    const newPersonaje = await Personaje.update(personajeData, {
+      where: {
+	id
+      }
+    })
+    console.log(newPersonaje)
+    res.status(200).json(personajeDto(newPersonaje))
+  }
+  catch(err) {
+    next(err)
+  }
+})
+
 module.exports = personajesController
